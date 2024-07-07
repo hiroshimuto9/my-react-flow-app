31fe16b3d8f882df168f222903b781c2aeedfee1
Hiroshi Muto <mhi6o461o@gmail.com>
2024-07-07 17:19:33 +0900

    core conceptの内容を実装

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,46 @@ import ReactFlow, {
   useNodesState,
   useEdgesState,
   addEdge,
+  applyEdgeChanges,
+  applyNodeChanges,
 } from "reactflow";
 
 import "reactflow/dist/style.css";
 
 const initialNodes = [
-  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
-  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+  {
+    id: "1",
+    type: "input",
+    data: { label: "Input Node" },
+    position: { x: 250, y: 25 },
+  },
+
+  {
+    id: "2",
+    // you can also pass a React component as a label
+    data: { label: <div>Default Node</div> },
+    position: { x: 100, y: 125 },
+  },
+  {
+    id: "3",
+    type: "output",
+    data: { label: "Output Node" },
+    position: { x: 250, y: 250 },
+  },
 ];
-const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+
+const initialEdges = [
+  { id: "e1-2", source: "1", target: "2" },
+  { id: "e2-3", source: "2", target: "3", animated: true },
+];
 
 export default function App() {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
   const onConnect = useCallback(
-    (params) => setEdges((eds) => addEdge(params, eds)),
+    (connection) =>
+      setEdges((eds) => addEdge({ ...connection, animated: true }, eds)),
     [setEdges]
   );
 
@@ -35,7 +59,6 @@ export default function App() {
         onConnect={onConnect}
       >
         <Controls />
-        <MiniMap />
         <Background variant="dots" gap={12} size={1} />
       </ReactFlow>
     </div>
