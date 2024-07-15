0fe7c5e881ce668ce83ddf95394038465c6e23a9
Hiroshi Muto <mhi6o461o@gmail.com>
2024-07-15 15:32:49 +0900

    カスタムノードにコネクションを追加

--- src/App.jsx
+++ src/App.jsx
@@ -48,8 +48,14 @@ const initialNodes = [
 ];
 
 const initialEdges = [
-  { id: "e1-2", source: "1", target: "2" },
-  { id: "e2-3", source: "2", target: "3", animated: true },
+  { id: "e1-2", source: "node-1", target: "2", sourceHandle: "a" },
+  {
+    id: "e2-3",
+    source: "node-1",
+    target: "3",
+    animated: true,
+    sourceHandle: "b",
+  },
 ];
 
 export default function App() {
