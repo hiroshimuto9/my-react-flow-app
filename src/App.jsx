1e10e48223f8ae003b756c2db43b224c0f24147d
Hiroshi Muto <mhi6o461o@gmail.com>
2024-07-20 17:02:28 +0900

    カスタムエッジラベルを実装

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,11 @@ import ReactFlow, {
 import "reactflow/dist/style.css";
 
 import TextUpdaterNode from "./components/TextUpdaterNode";
+import CustomEdge from "./components/CustomEdge";
+
 const nodeTypes = { textUpdater: TextUpdaterNode };
+const edgeTypes = { "custom-edge": CustomEdge };
+
 const rfStyle = {
   backgroundColor: "#B8CEFF",
 };
@@ -22,40 +26,34 @@ const rfStyle = {
 const initialNodes = [
   {
     id: "1",
-    type: "input",
-    data: { label: "Input Node" },
-    position: { x: 250, y: 25 },
+    // type: "input",
+    data: { label: "Node1" },
+    position: { x: 100, y: 100 },
   },
 
   {
     id: "2",
     // you can also pass a React component as a label
-    data: { label: <div>Default Node</div> },
-    position: { x: 100, y: 125 },
+    data: { label: <div>Node2</div> },
+    position: { x: 100, y: 200 },
   },
   {
     id: "3",
-    type: "output",
-    data: { label: "Output Node" },
-    position: { x: 250, y: 250 },
+    // type: "output",
+    data: { label: "Node3" },
+    position: { x: 100, y: 300 },
   },
-  {
-    id: "node-1",
-    type: "textUpdater",
-    position: { x: 0, y: 0 },
-    data: { value: 123 },
-  },
+  // {
+  //   id: "node-1",
+  //   type: "textUpdater",
+  //   position: { x: 0, y: 0 },
+  //   data: { value: 123 },
+  // },
 ];
 
 const initialEdges = [
-  { id: "e1-2", source: "node-1", target: "2", sourceHandle: "a" },
-  {
-    id: "e2-3",
-    source: "node-1",
-    target: "3",
-    animated: true,
-    sourceHandle: "b",
-  },
+  { id: "e1-2", source: "1", target: "2", type: "custom-edge" },
+  { id: "e2-3", source: "2", target: "3", type: "custom-edge" },
 ];
 
 export default function App() {
@@ -63,16 +61,18 @@ export default function App() {
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
   const onConnect = useCallback(
-    (connection) =>
-      setEdges((eds) => addEdge({ ...connection, animated: true }, eds)),
+    (connection) => {
+      const edge = { ...connection, type: "custom-edge" };
+      setEdges((eds) => addEdge(edge, eds));
+    },
     [setEdges]
   );
-
   return (
     <ReactFlowProvider>
       <div style={{ width: "100vw", height: "100vh" }}>
         <ReactFlow
           nodeTypes={nodeTypes}
+          edgeTypes={edgeTypes}
           nodes={nodes}
           edges={edges}
           onNodesChange={onNodesChange}
